37b9f9147769cae07eb9b7fc6294611feb8eb0c2
archieLa <arturm.lach@gmail.com>
2020-12-01 22:56:17 -0700

    Initial check-in of module Disher

---
+++ Disher/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Disher.ViewModels;
+using Disher.Views;
+using Xamarin.Forms;
+
+namespace Disher
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
